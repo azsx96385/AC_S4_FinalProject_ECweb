00f3b4bfce34e816700367c834aecfc1653e3a6b
Wayne <azsx96385@gmail.com>
2019-07-24 19:14:03 +0800

    set controller-service | router-Router

---
+++ route/water_index.js
@@ -0,0 +1,13 @@
+const app = require("express");
+let saleModel = require("./admin/saleModel");
+let productModel = require("./admin/productModel");
+let marketingModel = require("./admin/marketingModel");
+
+//設定路由群組
+//admin===============================================
+//銷售模組
+app.use("/salemodel", saleModel);
+//產品模組
+app.use("/productmodel", productModel);
+//行銷模組
+app.use("/marketingmodel", marketingModel);
---
+++ services/admin/productModelService copy.js
@@ -0,0 +1,7 @@
+//引入套件 & model
+
+//設定 Service
+productModelService = {};
+
+//匯出 Service
+module.exports = productModelService;
