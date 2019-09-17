fd0c92471b5ab18bc2baaec4330862c245b87b8d
Wayne <azsx96385@gmail.com>
2019-09-17 17:44:04 +0800

    seperate seeder

--- seeders/20190807105106-create_fake_store_data2.js
+++ seeders/20190807105106-create_fake_store_data2.js
@@ -4,17 +4,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     // Store_categories |3種類別3C類 / 服飾類 / 食品類
-    queryInterface.bulkInsert("store_categories", [
-      { name: "3C類", createdAt: new Date(), updatedAt: new Date() },
-      { name: "服飾類", createdAt: new Date(), updatedAt: new Date() },
-      { name: "食品類", createdAt: new Date(), updatedAt: new Date() }
-    ]);
 
-    // Stores
-    // 3間店，各一個分類
-    // 	1. 分類-1|米國電子 | apple水貨都在這裡
-    // 	2. 分類-2|GG服飾 | 櫻花妹最愛
-    // 	3. 分類-3|韓國包子 | 最胡鬧的大餅都在這
     return queryInterface.bulkInsert("stores", [
       {
         StoreCategoryId: 1,
@@ -41,7 +31,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    queryInterface.bulkDelete("stores", null, {});
-    return queryInterface.bulkDelete("store_categories", null, {});
+    return queryInterface.bulkDelete("stores", null, {});
   }
 };
