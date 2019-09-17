2bce2b0908056259738eda6b6a0270f15e55938e
Wayne <azsx96385@gmail.com>
2019-09-17 18:17:39 +0800

    seperate seeder table Name 大寫

--- seeders/20190807105106-create_fake_store_data.js
+++ seeders/20190807105106-create_fake_store_data.js
@@ -4,8 +4,18 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     // Store_categories |3種類別3C類 / 服飾類 / 食品類
+    queryInterface.bulkInsert("store_categories", [
+      { name: "3C類", createdAt: new Date(), updatedAt: new Date() },
+      { name: "服飾類", createdAt: new Date(), updatedAt: new Date() },
+      { name: "食品類", createdAt: new Date(), updatedAt: new Date() }
+    ]);
 
-    return queryInterface.bulkInsert("Stores", [
+    // Stores
+    // 3間店，各一個分類
+    // 	1. 分類-1|米國電子 | apple水貨都在這裡
+    // 	2. 分類-2|GG服飾 | 櫻花妹最愛
+    // 	3. 分類-3|韓國包子 | 最胡鬧的大餅都在這
+    return queryInterface.bulkInsert("stores", [
       {
         StoreCategoryId: 1,
         name: "米國電子",
@@ -31,6 +41,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("stores", null, {});
+    queryInterface.bulkDelete("stores", null, {});
+    return queryInterface.bulkDelete("store_categories", null, {});
   }
 };
---
+++ seeders/20190807105148-create_fake_user_data.js
@@ -0,0 +1,70 @@
+"use strict";
+//假資料 - 米國電子|1位管理者|3位顧客
+const bcrypt = require("bcryptjs");
+const faker = require("faker");
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        StoreId: 1,
+        role: 1,
+        name: "root",
+        email: "root@example.com",
+        password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+        address: faker.address.streetAddress(),
+        resetPasswordToken: null,
+        resetPasswordExpires: null,
+        image: 'https://i.imgur.com/Uzs2ty3.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        emailVerf: 0
+      },
+      {
+        StoreId: 1,
+        role: 0,
+        name: "user1",
+        email: "user1@example.com",
+        password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+        address: faker.address.streetAddress(),
+        resetPasswordToken: null,
+        resetPasswordExpires: null,
+        image: 'https://i.imgur.com/Uzs2ty3.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        emailVerf: 0
+      },
+      {
+        StoreId: 1,
+        role: 0,
+        name: "user2",
+        email: "user2@example.com",
+        password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+        address: faker.address.streetAddress(),
+        resetPasswordToken: null,
+        resetPasswordExpires: null,
+        image: 'https://i.imgur.com/Uzs2ty3.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        emailVerf: 0
+      },
+      {
+        StoreId: 1,
+        role: 0,
+        name: "user3",
+        email: "user3@example.com",
+        password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+        address: faker.address.streetAddress(),
+        resetPasswordToken: null,
+        resetPasswordExpires: null,
+        image: 'https://i.imgur.com/Uzs2ty3.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        emailVerf: 0
+      }
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  }
+};
