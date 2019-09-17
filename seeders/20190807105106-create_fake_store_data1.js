f12ed64b892a6d55f37a0d7ab63f0ce4510da324
Wayne <azsx96385@gmail.com>
2019-09-17 17:47:03 +0800

    seperate seeder2

--- seeders/20190807105106-create_fake_store_data1.js
+++
@@ -1,17 +0,0 @@
-"use strict";
-
-//注意| seed 是沒有經過 model 處理的
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    // Store_categories |3種類別3C類 / 服飾類 / 食品類
-    return queryInterface.bulkInsert("store_categories", [
-      { name: "3C類", createdAt: new Date(), updatedAt: new Date() },
-      { name: "服飾類", createdAt: new Date(), updatedAt: new Date() },
-      { name: "食品類", createdAt: new Date(), updatedAt: new Date() }
-    ]);
-  },
-
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("store_categories", null, {});
-  }
-};
