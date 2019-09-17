b1148633471460a55054d1464ed3e5271ebda9d5
Wayne <azsx96385@gmail.com>
2019-09-17 17:40:05 +0800

    add relation on store with store category2

--- models/store_category.js
+++ models/store_category.js
@@ -1,10 +1,15 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Store_category = sequelize.define('Store_category', {
-    name: DataTypes.STRING
-  }, {});
+  const Store_category = sequelize.define(
+    "Store_category",
+    {
+      name: DataTypes.STRING
+    },
+    {}
+  );
   Store_category.associate = function(models) {
     // associations can be defined here
+    Store_category.hasMany(models.Store);
   };
   return Store_category;
 };
