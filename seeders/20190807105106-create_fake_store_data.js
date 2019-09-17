88a2d832d442c8efff2dc5c60e274298ad243d3a
Wayne <azsx96385@gmail.com>
2019-09-17 18:24:46 +0800

    seperate seeder store category 註解

--- seeders/20190807105106-create_fake_store_data.js
+++ seeders/20190807105106-create_fake_store_data.js
@@ -4,18 +4,18 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     // Store_categories |3種類別3C類 / 服飾類 / 食品類
-    queryInterface.bulkInsert("store_categories", [
-      { name: "3C類", createdAt: new Date(), updatedAt: new Date() },
-      { name: "服飾類", createdAt: new Date(), updatedAt: new Date() },
-      { name: "食品類", createdAt: new Date(), updatedAt: new Date() }
-    ]);
+    // queryInterface.bulkInsert("Store_categories", [
+    //   { name: "3C類", createdAt: new Date(), updatedAt: new Date() },
+    //   { name: "服飾類", createdAt: new Date(), updatedAt: new Date() },
+    //   { name: "食品類", createdAt: new Date(), updatedAt: new Date() }
+    // ]);
 
     // Stores
     // 3間店，各一個分類
     // 	1. 分類-1|米國電子 | apple水貨都在這裡
     // 	2. 分類-2|GG服飾 | 櫻花妹最愛
     // 	3. 分類-3|韓國包子 | 最胡鬧的大餅都在這
-    return queryInterface.bulkInsert("stores", [
+    return queryInterface.bulkInsert("Stores", [
       {
         StoreCategoryId: 1,
         name: "米國電子",
@@ -41,7 +41,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    queryInterface.bulkDelete("stores", null, {});
-    return queryInterface.bulkDelete("store_categories", null, {});
+    queryInterface.bulkDelete("Stores", null, {});
+    return queryInterface.bulkDelete("Store_categories", null, {});
   }
 };
