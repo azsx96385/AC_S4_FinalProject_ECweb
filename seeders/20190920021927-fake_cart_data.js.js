eb6583db597492f25bff6d0c137756e170c99b1c
Wayne <azsx96385@gmail.com>
2019-09-20 10:21:18 +0800

    add cart fake data

---
+++ seeders/20190920021927-fake_cart_data.js.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    //cart cartItems
+    //三台購物車，隨機配3項商品，數量1-3隨機
+    queryInterface.bulkInsert("Carts", [
+      {
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+    return queryInterface.bulkInsert(
+      "Cart_items",
+      [1, 1, 1, 2, 2, 2, 0, 0, 0].map((item, index) => ({
+        CartId: item,
+        ProductId: Math.floor(Math.random() * 7) + 1,
+        quantity: Math.floor(Math.random() * 3) + 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }))
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete("Cart_items", null, {});
+    return queryInterface.bulkDelete("Carts", null, {});
+  }
+};
