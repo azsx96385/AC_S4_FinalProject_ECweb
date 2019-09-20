292e83fa8cce3abf7d47f69496ac188c2a79fc10
Wayne <azsx96385@gmail.com>
2019-09-20 11:52:27 +0800

    seperate product & comment1

--- seeders/20190920021927-fake_cart_data.js
+++ seeders/20190920021927-fake_cart_data.js
@@ -4,30 +4,33 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     //cart cartItems
     //三台購物車，隨機配3項商品，數量1-3隨機
-    queryInterface.bulkInsert("Carts", [
-      {
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]);
-    return queryInterface.bulkInsert(
-      "Cart_items",
-      [1, 1, 1, 2, 2, 2, 3, 3, 3].map((item, index) => ({
-        CartId: item,
-        ProductId: Math.floor(Math.random() * 7) + 1,
-        quantity: Math.floor(Math.random() * 3) + 1,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
-    );
+    queryInterface
+      .bulkInsert("Carts", [
+        {
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ])
+      .then(data => {
+        return queryInterface.bulkInsert(
+          "Cart_items",
+          [1, 1, 1, 2, 2, 2, 3, 3, 3].map((item, index) => ({
+            CartId: item,
+            ProductId: Math.floor(Math.random() * 7) + 1,
+            quantity: Math.floor(Math.random() * 3) + 1,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }))
+        );
+      });
   },
 
   down: (queryInterface, Sequelize) => {
