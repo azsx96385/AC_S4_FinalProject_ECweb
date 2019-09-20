2e9033176466ef8360166d3e2261494beb094898
Wayne <azsx96385@gmail.com>
2019-09-20 11:56:55 +0800

    seperate product & comment3

--- seeders/20190920030404-fake_order_data.js
+++ seeders/20190920030404-fake_order_data.js
@@ -4,32 +4,35 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     //Orders OrderItems OrderStatus payment shipment
 
-    queryInterface.bulkInsert(
-      "Orders",
-      [2, 3, 4].map((item, index) => ({
-        UserId: item,
-        StoreId: 1,
-        OrderStatusId: Math.floor(Math.random() * 2) + 1,
-        name: "water",
-        amount: 2000,
-        phone: "06-2626255",
-        address: faker.address.streetAddress(),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
-    );
-    //Order-item 紀錄訂單中商品
-    return queryInterface.bulkInsert(
-      "Order_items",
-      [1, 2, 3].map((item, index) => ({
-        ProductId: Math.floor(Math.random() * 7) + 1,
-        OrderId: 1,
-        quantity: Math.floor(Math.random() * 3),
-        price: 2000,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
-    );
+    return queryInterface
+      .bulkInsert(
+        "Orders",
+        [2, 3, 4].map((item, index) => ({
+          UserId: item,
+          StoreId: 1,
+          OrderStatusId: Math.floor(Math.random() * 2) + 1,
+          name: "water",
+          amount: 2000,
+          phone: "06-2626255",
+          address: faker.address.streetAddress(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }))
+      )
+      .then(data => {
+        //Order-item 紀錄訂單中商品
+        return queryInterface.bulkInsert(
+          "Order_items",
+          [1, 2, 3].map((item, index) => ({
+            ProductId: Math.floor(Math.random() * 7) + 1,
+            OrderId: 1,
+            quantity: Math.floor(Math.random() * 3),
+            price: 2000,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }))
+        );
+      });
   },
 
   down: (queryInterface, Sequelize) => {
