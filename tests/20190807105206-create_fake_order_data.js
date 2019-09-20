81060c60bff29b4732737e84f7e9f12064a16133
Wayne <azsx96385@gmail.com>
2019-09-20 10:25:23 +0800

    rename cart seed

--- tests/20190807105206-create_fake_order_data.js
+++ tests/20190807105206-create_fake_order_data.js
@@ -7,32 +7,6 @@
 const faker = require("faker");
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    //cart cartItems
-    //三台購物車，隨機配3項商品，數量1-3隨機
-    queryInterface.bulkInsert("Carts", [
-      {
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]);
-    queryInterface.bulkInsert(
-      "Cart_items",
-      [1, 1, 1, 2, 2, 2, 0, 0, 0].map((item, index) => ({
-        CartId: 1,
-        ProductId: Math.floor(Math.random() * 7) + 1,
-        quantity: Math.floor(Math.random() * 3) + 1,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
-    );
     // shipment_statuses shipment_types
     queryInterface.bulkInsert(
       "Shipment_statuses",
